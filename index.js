8a67366922850fc4ce56c097fff6033bc243c710
Rajasimha Reddy <rajasimha5599@gmail.com>
2022-04-09 16:15:38 +0530

    add backgrounds

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ Block.prototype.init = function() {
     for (let i = 1; i <= this.count; i++) {
         const iElem = document.createElement("div");
         iElem.classList.add("plain-block");
-        iElem.style.width = 100 / this.count - 10 + "vw";
-        iElem.style.height = 100 / this.count - 10 + "vw";
+        iElem.style.width = 100 / this.count - 1 + "vw";
+        iElem.style.height = 100 / this.count - 1 + "vw";
         iElem.dataset.value = i;
         fragment.appendChild(iElem);
     }
     this.element.appendChild(fragment);
+    setHighScore();
 }
 
 Block.prototype.glow = function() {
@@ -37,16 +38,21 @@ Block.prototype.glow = function() {
 
 Block.prototype.onClick = function(e) {
     const value = Number(e.target.dataset.value);
-    if (this.glowOrder.shift() !== value) {
+    if (this.glowOrder[0] !== value) {
         blink(e.target, 'red').then(() => {});
         this.element.classList.add('shake');
         setTimeout(() => {
             this.element.classList.remove('shake');
-        }, 1000)
+        }, 1000);
+        storeHighScore(this.score);
         this.score = 0;
         this.frequency = 1;
-        updateScores(this.score)
+        this.glowOrder = [];
+        updateScores(this.score);
+        startButton.disabled = false;
+        return;
     }
+    this.glowOrder.shift();
     if (this.glowOrder.length === 0) {
         ++this.score;
         ++this.frequency;
@@ -64,15 +70,28 @@ const blink = (el, color) => {
         let element = el
         element.style.backgroundColor = color;
         setTimeout(() => {
-            element.style = '';
-            resolve();
+            element.style.backgroundColor = '';
+            setTimeout(() => resolve(), 300);
         }, 500)
     })
 }
 
 const updateScores = (score) => {
     const scoreDiv = document.querySelector('.score');
+    scoreDiv.innerHTML = score || 0;
+}
+
+const setHighScore = () => {
+    let score = localStorage.getItem('highScore') || 0;
+    const scoreDiv = document.querySelector('.high-score');
     scoreDiv.innerHTML = score;
+}
+
+const storeHighScore = (score) => {
+    let existingScore = localStorage.getItem('highScore') || 0;
+    if (parseInt(existingScore) < parseInt(score)) {
+        localStorage.setItem('highScore', score);
+    }
 }
 
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
@@ -82,4 +101,6 @@ let gameBlocks = new Block("#blocks", 5, () => {});
 let startButton = document.querySelector('.start');
 startButton.addEventListener("click", () => {
     gameBlocks.glow();
+    setHighScore();
+    startButton.disabled = true;
 })
